95ec79764489c22f713907482c8159ac8314b297
nashaofu <diaocheng@outlook.com>
2018-05-26 17:20:58 +0800

    升级依赖，修改通知提示引用库

--- src/preload/mainWin/notify.js
+++ src/preload/mainWin/notify.js
@@ -1,37 +1,29 @@
-import path from 'path'
-import notifier from 'node-notifier'
-import { ipcRenderer, remote } from 'electron'
-
-notifier.on('click', () => {
-  ipcRenderer.send('MAINWIN:window-show')
-})
+import { ipcRenderer } from 'electron'
 
 let notify
 export default message => {
-  notifier.notify({
-    title: '钉钉',
-    message,
-    icon: `file://${path.join(remote.app.getAppPath(), './icon/128x128.png')}`
-  }, err => {
-    if (!err) return
-    if (notify instanceof Notification) {
-      notify.close()
-    }
-    if (Notification.permission === 'granted') {
-      notify = new Notification('钉钉', {
-        body: message,
-        icon: 'https://g.alicdn.com/dingding/web/0.1.8/img/logo.png'
-      })
-    } else if (Notification.permission !== 'denied') {
-      Notification.requestPermission(permission => {
-        // 如果用户同意，就可以向他们发送通知
-        if (permission === 'granted') {
-          notify = new Notification('钉钉', {
-            body: message,
-            icon: 'https://g.alicdn.com/dingding/web/0.1.8/img/logo.png'
-          })
-        }
-      })
-    }
-  })
+  if (notify instanceof Notification) {
+    notify.close()
+    notify = null
+  }
+  if (Notification.permission === 'granted') {
+    notify = new Notification('钉钉', {
+      body: message,
+      icon: 'https://g.alicdn.com/dingding/web/0.1.8/img/logo.png',
+      tag: 'dingtalk'
+    })
+    notify.addEventListener('click', () => ipcRenderer.send('MAINWIN:window-show'))
+  } else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(permission => {
+      // 如果用户同意，就可以向他们发送通知
+      if (permission === 'granted') {
+        notify = new Notification('钉钉', {
+          body: message,
+          icon: 'https://g.alicdn.com/dingding/web/0.1.8/img/logo.png',
+          tag: 'dingtalk'
+        })
+        notify.addEventListener('click', () => ipcRenderer.send('MAINWIN:window-show'))
+      }
+    })
+  }
 }
